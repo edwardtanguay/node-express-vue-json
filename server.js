b1d6809db8118aa09aacbfd47a6a3702c3cc4914
Edward Tanguay (wk) <edward@tanguay.info>
2020-09-14 14:43:51 +0200

    Serve an .ejs file

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3001;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './public/pages'));
+
+const staticDirectory = path.join(__dirname, './public');
+app.use(express.static(staticDirectory));
+
 app.get('/', (req, res) => {
-	res.send('hello app');
+	res.render('index', { pageTitle: 'Flashcards' });
 });
 
 app.listen(port, () => {
